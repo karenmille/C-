8d08d903dcc2d85b3a21981bc1f024752ee2fef6
karen miller <morenk@mail.broward.edu>
2018-06-06 14:18:11 -0400

    Add files via upload

---
+++ salesCalculator/lab1/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace lab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnCalculatr = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.txtEmployeeName = new System.Windows.Forms.TextBox();
+            this.txtSaleAmount = new System.Windows.Forms.TextBox();
+            this.lblGrossPay = new System.Windows.Forms.Label();
+            this.lblDeduction = new System.Windows.Forms.Label();
+            this.lblNetPay = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(22, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(154, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter Employee Name:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(22, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(204, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Enter Employee Slaes Amount:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(32, 203);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Gross Pay:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(32, 256);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Deductions:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(32, 300);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(60, 15);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Net Pay:";
+            // 
+            // btnCalculatr
+            // 
+            this.btnCalculatr.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnCalculatr.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculatr.Location = new System.Drawing.Point(25, 138);
+            this.btnCalculatr.Name = "btnCalculatr";
+            this.btnCalculatr.Size = new System.Drawing.Size(159, 42);
+            this.btnCalculatr.TabIndex = 5;
+            this.btnCalculatr.Text = "Calculate Pay";
+            this.btnCalculatr.UseVisualStyleBackColor = true;
+            this.btnCalculatr.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.btnClear.FlatAppearance.BorderColor = System.Drawing.Color.Blue;
+            this.btnClear.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(199, 138);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(159, 42);
+            this.btnClear.TabIndex = 6;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // txtEmployeeName
+            // 
+            this.txtEmployeeName.Location = new System.Drawing.Point(199, 28);
+            this.txtEmployeeName.Name = "txtEmployeeName";
+            this.txtEmployeeName.Size = new System.Drawing.Size(146, 20);
+            this.txtEmployeeName.TabIndex = 7;
+            this.txtEmployeeName.Text = "ka";
+            // 
+            // txtSaleAmount
+            // 
+            this.txtSaleAmount.Location = new System.Drawing.Point(245, 67);
+            this.txtSaleAmount.Name = "txtSaleAmount";
+            this.txtSaleAmount.Size = new System.Drawing.Size(100, 20);
+            this.txtSaleAmount.TabIndex = 8;
+            // 
+            // lblGrossPay
+            // 
+            this.lblGrossPay.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblGrossPay.Location = new System.Drawing.Point(170, 196);
+            this.lblGrossPay.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblGrossPay.Name = "lblGrossPay";
+            this.lblGrossPay.Size = new System.Drawing.Size(91, 29);
+            this.lblGrossPay.TabIndex = 12;
+            // 
+            // lblDeduction
+            // 
+            this.lblDeduction.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblDeduction.Location = new System.Drawing.Point(170, 239);
+            this.lblDeduction.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblDeduction.Name = "lblDeduction";
+            this.lblDeduction.Size = new System.Drawing.Size(94, 31);
+            this.lblDeduction.TabIndex = 13;
+            // 
+            // lblNetPay
+            // 
+            this.lblNetPay.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblNetPay.Location = new System.Drawing.Point(170, 291);
+            this.lblNetPay.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblNetPay.Name = "lblNetPay";
+            this.lblNetPay.Size = new System.Drawing.Size(94, 32);
+            this.lblNetPay.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.ClientSize = new System.Drawing.Size(417, 394);
+            this.Controls.Add(this.lblNetPay);
+            this.Controls.Add(this.lblDeduction);
+            this.Controls.Add(this.lblGrossPay);
+            this.Controls.Add(this.txtSaleAmount);
+            this.Controls.Add(this.txtEmployeeName);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnCalculatr);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnCalculatr;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.TextBox txtEmployeeName;
+        private System.Windows.Forms.TextBox txtSaleAmount;
+        private System.Windows.Forms.Label lblGrossPay;
+        private System.Windows.Forms.Label lblDeduction;
+        private System.Windows.Forms.Label lblNetPay;
+    }
+}
+
---
+++ salesCalculator/lab1/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab1
+{
+    public partial class Form1 : Form
+    {
+        //class level variables
+        double salesAmount, grossPay, netPay, totalDeductions, totalComission;
+       
+        //declare constans
+        const double SALE_COMMISSION = 0.06;
+        const double DEDUCTION = 0.18;
+        const double BASE_PAY = 900.0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            //init variables
+            salesAmount = grossPay = netPay = totalDeductions = totalComission = 0.00;
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            Clear();
+        }
+
+        private void Clear()
+        {
+            txtEmployeeName.Clear();
+            txtSaleAmount.Clear();
+            lblDeduction.Text = "";
+            lblGrossPay.Text = "";
+            lblNetPay.Text = "";
+            salesAmount = grossPay = netPay = totalDeductions = totalComission = 0.00;
+
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string str;
+            salesAmount = Convert.ToDouble(txtSaleAmount.Text);
+            if (txtEmployeeName.Text.Length > 0)
+            {
+               str = txtEmployeeName.Text + ", Please Enter A Positive Amount! ";
+            }
+            else
+            {
+                str = "Please Enter A Positive Amount! ";
+            }
+            if (salesAmount < 0)
+            {
+                MessageBox.Show(str, "Invalid Amount", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Clear();
+                return;
+
+            }
+            totalComission = salesAmount * SALE_COMMISSION;
+            grossPay = BASE_PAY + totalComission;
+            totalDeductions = grossPay * DEDUCTION;
+            netPay = grossPay - totalDeductions;
+
+            lblDeduction.Text = String.Format("{0:C}", Convert.ToInt32(totalDeductions)); 
+            lblGrossPay.Text = String.Format("{0:C}", Convert.ToInt32(grossPay));
+            lblNetPay.Text = String.Format("{0:C}", Convert.ToInt32(netPay));
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
